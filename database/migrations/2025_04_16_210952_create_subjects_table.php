ab7cbca536aeb7f8861d7eb4efcf2dafd50b20db
shahenda8 <shahendatarek33@gmail.com>
2025-04-29 18:25:55 +0300

    add attend column and matrial

--- database/migrations/2025_04_16_210952_create_subjects_table.php
+++ database/migrations/2025_04_16_210952_create_subjects_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('stage_id')->constrained()->cascadeOnDelete();
             $table->string('name');
+            $table->string('materials');
             $table->timestamps();
         });
     }
